f8463a981cb0cc613ab8867fb40564cc72e2fe6d
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-04-11 20:40:56 -0300

    :construction: feat: eventos created

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.contrib import auth
 
 def cadastro(request):
     if request.method == "GET":
@@ -27,3 +28,19 @@ def cadastro(request):
         messages.add_message(request, constants.SUCCESS, 'Usuário salvo com sucesso.')  
         user.save()
         return redirect(reverse('login'))
+    
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get('senha')
+
+        user = auth.authenticate(username=username, password=senha)
+
+        if not user:
+            messages.add_message(request, constants.ERROR, 'Username ou senha inválidos')
+            return redirect(reverse('login'))
+        
+        auth.login(request, user)
+        return redirect('/eventos/novo_evento/')
