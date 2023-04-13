28f8547fb4bf6e38a6f3cb550ac0282eeb561fbf
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2023-04-13 09:02:42 -0300

    :construction: feat: gerenciar_evento created

--- eventos/urls.py
+++ eventos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('novo_evento/', views.novo_evento, name="novo_evento"),
+    path("novo_evento/", views.novo_evento, name="novo_evento"),
+    path("gerenciar_evento/", views.gerenciar_evento, name="gerenciar_evento"),
 ]
